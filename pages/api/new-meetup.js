2dab13628ab9b8dbf4f276dde02f279d23a59557
mahi1212 <mahi121.mr@gmail.com>
2022-04-19 16:31:53 +0600

    added fetching api from database

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+import { MongoClient } from 'mongodb'
+// api/new-meetup
+// POST/ api/new-meetup
+
+async function handler(req, res){
+    if(req.method === 'POST'){
+        const data = req.body
+        // Connect to server side
+        const client = await MongoClient.connect('mongodb+srv://meetupAdmin:KgBeVHfXJAfR9yns@cluster0.tecyb.mongodb.net/meetups?retryWrites=true&w=majority')
+        const db = client.db()
+        const meetupsCollection = db.collection('meetups')
+        const result = await meetupsCollection.insertOne(data)
+        // console.log(result)
+        client.close()
+
+        res.status(201).json({message: 'Meetups Inserted'})
+        
+    }
+}
+
+export default handler;
